830155a4131baec619b75fdb6490141ed53aa13e
melanoman <mhnicholson@yahoo.com>
2025-08-08 11:41:46 -0700

    wireframe for playPause control

--- src/RPS.js
+++ src/RPS.js
@@ -5,8 +5,29 @@ import playOn from './icon/rps_playOn.svg';
 import playOff from './icon/rps_playOff.svg';
 import glassOn from './icon/rps_glassOn.svg';
 import glassOff from './icon/rps_glassOff.svg';
+import pausePink from './icon/pausePink.svg';
+import pauseGreen from './icon/pauseGreen.svg';
+import playPink from './icon/playPink.svg';
+import playGreen from './icon/playGreen.svg';
 const JOIN = true;
 const LEAVE = false;
+const IDLE = 0;
+const SELECTING = 1;
+
+function timer() {} // TODO display timer
+
+function pause(props, paused, setTo, setPaused) {
+  setPaused(setTo); //TODO request server pause or unpause and only set flag on server response
+}
+
+function statusBar(playing, phase) {
+  if(playing) {
+    if (phase == IDLE) return <div class="subtitle">Player (paused)</div>
+    return <div class="subtitle">Player (make your choice) </div>
+  }
+  if (phase == IDLE) return <div class="subtitle">Observer (paused)</div>
+  return <div class="subtitle">Observer (players are selecting)</div>
+}
 
 function play(props, playing, setPlaying, join) {
   if(playing === join) {
@@ -16,18 +37,24 @@ function play(props, playing, setPlaying, join) {
   setPlaying(join);
 }
 
-function playControl(props, playing, setPlaying) {
+function playControl(props, playing, setPlaying, paused, setPaused) {
   return <div>
     {imageButton(() => play(props, playing, setPlaying, JOIN), playing? playOn: playOff, "join")}
     {imageButton(() => play(props, playing, setPlaying, LEAVE), playing? glassOff: glassOn, "watch")}
+    {imageButton(() => pause(props, paused, false, setPaused), paused? playPink: playGreen, "pause")}
+    {imageButton(() => pause(props, paused, true, setPaused), paused? pauseGreen: pausePink, "pause")}
+    {timer()}
   </div>;
 }
 
 export function RPSPanel(props) {
   const [playing, setPlaying] = useState(false);
+  const [status, setStatus] = useState(IDLE);
+  const [paused, setPaused] = useState(true);
 
   return <div>
     <div class="title">Roshambo (Rock Paper Scissors)</div>
-    {playControl(props, playing, setPlaying)}
+    {playControl(props, playing, setPlaying, paused, setPaused)}
+    {statusBar(playing, status)}
   </div>
 }

d1581c2128767af76127aef67c407588162e7452
Mel Nicholson <mhnicholson@yahoo.com>
2026-06-29 11:41:56 -0700

    add undo/redo/redoAll

--- src/xx18/xxSeater.js
+++ src/xx18/xxSeater.js
@@ -1,38 +1,44 @@
 import React, {useState, useEffect} from 'react';
 import '../util.css';
 
-import { smallImageButton, imageButton, isVoid, onEnter } from '../util.js';
+import { smallImageButton, imageButton, bigImageButton, isVoid, onEnter } from '../util.js';
 
 import add from '../icon/add.svg';
 import check from '../icon/check.svg';
 import cancel from '../icon/cancel.svg';
 import pencil from '../icon/pencil.svg';
+import play from '../icon/playGreen.svg';
 
 const setters = {}
 
-function changePlayerName() { alert("TODO changeName") }
-function abortPlayerNameChange() {alert("TODO abortNameChange") }
+function changePlayerName(props, oldName, newName) {
+  setters.setNewPlayerName("");
+  setters.setEditingPlayer(null)
+  props.net.put(props.net, "renamePlayer/"+props.board.name+"/"+oldName+"/"+newName)
+}
 
-function EditPlayerNamePanel(props, gameName, oldPlayerName, newPlayerName) {
+function abortPlayerNameChange() { setters.setEditingPlayer(false) }
+
+function EditPlayerNamePanel(props, oldPlayerName, newPlayerName) {
   return <div>
     <div class="subtitle">Changing name of {oldPlayerName}</div>
     <input type="text" value={newPlayerName} class="med-text"
         onChange={(e) => setters.setNewPlayerName(e.target.value)}
         onKeyDown={(e) => onEnter(e.key,
-                   () => changePlayerName(props, gameName, oldPlayerName, newPlayerName))}/>
+                   () => changePlayerName(props, oldPlayerName, newPlayerName))}/>
     <div>
-      {imageButton(() => changePlayerName(props, gameName, oldPlayerName, newPlayerName), check, "ok")}
+      {imageButton(() => changePlayerName(props, oldPlayerName, newPlayerName), check, "ok")}
       {imageButton(() => abortPlayerNameChange(), cancel, "cancel")}
     </div>
   </div>
 }
 
 function playerNameEdit(props, player) {
-  alert("TODO edit name screen")
+  setters.setEditingPlayer(player);
 }
 
 function playerEditButton(props, player) {
-  return smallImageButton(() => playerNameEdit(props, player), pencil, "edit");
+  return smallImageButton(() => playerNameEdit(props, player.name), pencil, "edit");
 }
 
 function PlayerNameDisplay(props, player) {
@@ -58,16 +64,28 @@ function AddPlayerLine(props, newPlayerName) {
   </div>
 }
 
+function startGame(props, shuffle) {
+  props.net.put(props.net, "startGame/"+props.board.name+"/"+shuffle, props.board.name, shuffle)
+}
+
 export function Seater(props) {
   const [newPlayerName, setNewPlayerName] = useState(null);
+  const [editingPlayer, setEditingPlayer] = useState(null);
+  const [shuffle, setShuffle] = useState(false);
 
   setters.setNewPlayerName = setNewPlayerName;
+  setters.setEditingPlayer = setEditingPlayer;
+  setters.setShuffle = setShuffle;
 
   if(isVoid(props.board)) return <div class="error">Error: No Game to display</div>
+  if(!isVoid(editingPlayer)) return <div>{EditPlayerNamePanel(props, editingPlayer, newPlayerName)}</div>
   return <div>
     {props.board.players.map((player) => PlayerNameDisplay(props, player))}
     <div>{ AddPlayerLine(props, newPlayerName) }</div>
-    <div>SHUFFLE CHECKBOX HERE</div>
-    <div>START GAME HERE</div>
+    <div>
+      <input type="checkbox" checked={shuffle} onChange={(e) => setters.setShuffle(e.target.checked)} />
+      Shuffle on Start
+    </div>
+    <div>{bigImageButton(() => startGame(props, shuffle), play, "startGame")}</div>
   </div>
 }
